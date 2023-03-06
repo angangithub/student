11a3f5c1e26f94bfcbfa176eb222def9142ca5d7
86153 <1219929852@qq.com>
2023-03-06 17:47:41 +0800

    init

--- CRUD-jpa/src/main/java/com/example/domin/Courses.java
+++ CRUD-jpa/src/main/java/com/example/domin/Courses.java
@@ -3,11 +3,11 @@ package com.example.domin;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "courses")
