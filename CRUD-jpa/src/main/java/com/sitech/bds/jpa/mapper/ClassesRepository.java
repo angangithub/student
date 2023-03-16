3fcb2a1a1a2a07ba7728dfddf28356d963f23985
86153 <1219929852@qq.com>
2023-03-16 10:06:07 +0800

    swagger

--- CRUD-jpa/src/main/java/com/sitech/bds/jpa/mapper/ClassesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sitech.bds.jpa.mapper;
-
-import com.sitech.bds.jpa.domin.Classes;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassesRepository extends JpaRepository<Classes, Long> {
-}
--- CRUD-jpa/src/main/java/com/sitech/bds/jpa/mapper/CoursesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sitech.bds.jpa.mapper;
-
-import com.sitech.bds.jpa.domin.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoursesRepository extends JpaRepository<Courses, Long> {
-}
